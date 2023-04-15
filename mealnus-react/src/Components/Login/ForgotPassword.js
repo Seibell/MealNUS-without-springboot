86dec60ac04db1b3ba15833e00c3b08b4a6737cd
Ong Chang Hui <DarienOng@outlook.sg>
2023-04-15 16:22:44 +0800

    amend

--- mealnus-react/src/Components/Login/ForgotPassword.js
+++ mealnus-react/src/Components/Login/ForgotPassword.js
@@ -1,18 +1,62 @@
-import { Input, TextField } from "@mui/material";
+import React from "react";
+import {
+  Button,
+  TextField,
+  Typography,
+  Box,
+  Container,
+  Link,
+} from "@mui/material";
+import { styled } from "@mui/system";
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  minHeight: "100vh",
+  padding: theme.spacing(2),
+}));
+
+const StyledForm = styled("form")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  maxWidth: 400,
+}));
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  margin: theme.spacing(2, 0),
+}));
 
 function ForgotPassword() {
-    return (
-      <div>
-        <h1>Forgot Password</h1>
-        <form>
-            <TextField>
-                <Input type="email" placeholder="Email" />
-            </TextField>
-        </form>
-        <button>Send Email</button>
-        <a href="/">Back</a>
-      </div>
-    );
-  }
-  
-  export default ForgotPassword;
+  return (
+    <StyledContainer>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Forgot Password
+      </Typography>
+      <StyledForm>
+        <StyledTextField
+          label="Email"
+          type="email"
+          variant="outlined"
+          fullWidth
+        />
+        <StyledButton variant="contained" color="primary" fullWidth>
+          Send Email
+        </StyledButton>
+      </StyledForm>
+      <Box mt={2}>
+        <Link href="/" variant="body2">
+          Back
+        </Link>
+      </Box>
+    </StyledContainer>
+  );
+}
+
+export default ForgotPassword;
